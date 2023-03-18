dd367fd6d9c6d34471499802a79482c4387fa78a
Kaurx14 <99817804+Kaurx14@users.noreply.github.com>
2023-03-18 11:34:40 +0200

    order function ready

--- Inventory.cs
+++ Inventory.cs
@@ -11,10 +11,11 @@ namespace mhgConsoleApp
         public static int pepperoni = 10;
         public static int rukola = 10;
         public static int juust = 10;
+        public static int tomatikaste = 10;
 
         public static string GetInventory()
         {
-            string inventory = "Poes olevad koostisosad: \n \n" + "Pepperoni: " + pepperoni + "\n Rukola: " + rukola + "\n Juust: " + juust;
+            string inventory = "Poes olevad koostisosad: \n \n" + "Pepperoni: " + pepperoni + "\n Rukola: " + rukola + "\n Juust: " + juust + "\n Tomatikaste: " + tomatikaste;
 
             return inventory;
         }
--- Order.cs
+++ Order.cs
@@ -8,16 +8,29 @@ namespace mhgConsoleApp
 {
     public class Order
     {
-        public static void OnPizzaBought()
+        public void OnPizzaBought(Pizza orderedPizza)
         {
             Money.money += 1;
 
+            var koostisosad = orderedPizza.koostisosadList;
+
+            if(koostisosad.Contains(Pizza.koostisosad.pepperoni))
+            {
+                Inventory.pepperoni -= 1;
+            }
+            if (koostisosad.Contains(Pizza.koostisosad.juust))
+            {
+                Inventory.juust -= 1;
+            }
+            if (koostisosad.Contains(Pizza.koostisosad.tomatikaste))
+            {
+                Inventory.tomatikaste -= 1;
+            }
+            if (koostisosad.Contains(Pizza.koostisosad.rukola))
+            {
+                Inventory.tomatikaste -= 1;
+            }
 
         }
-
-        //public Pizza.koostisosad[] GetIngredientsOfPizza(Pizza orderedPizza)
-        //{
-        //    return orderedPizza.koostisosadList;
-        //}
     }
 }
