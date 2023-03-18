fe5d9ae3e90f332add669fd8e4a8d57ff5819b95
unknown <kaur.vadi@gmail.com>
2023-03-18 10:43:15 +0200

    ok

---
+++ käsklus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mhgConsoleApp
+{
+    public static class käsklus
+    {
+        public static string[] commands = { "MENU", "INVENTORY", "MONEY", "ORDER", "RESTOCK", "ADD PIZZA", "HISTORY", "SOLD" };
+        public static string DetermineCommand(string command)
+        {
+            command = command.ToUpper();
+
+            if (commands.Contains(command))
+                return command;
+
+
+
+            return null;
+        }
+    }
+}
