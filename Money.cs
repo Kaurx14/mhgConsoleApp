2cfa298f9e836e92ffabcbec28196cebd6a07401
Kaurx14 <99817804+Kaurx14@users.noreply.github.com>
2023-03-18 11:08:36 +0200

    money class

---
+++ Money.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mhgConsoleApp
+{
+    public class Money
+    {
+        public static int money = 0;
+
+        public static string GetMoney()
+        {
+            string raha = "Poes olev raha: " + money + "€";
+            return raha;
+        }
+    }
+}
