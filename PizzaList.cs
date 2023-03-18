85a508551eca97102f6598489706363b82b74503
Kaurx14 <99817804+Kaurx14@users.noreply.github.com>
2023-03-18 10:56:26 +0200

    GetPizza n others

---
+++ PizzaList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mhgConsoleApp
+{
+    public class PizzaList
+    {
+        public List<Pizza> pizzaList = new List<Pizza>();
+        public PizzaList()
+        {
+            pizzaList.Add(new Pizza("Pepperoni", 5.00f, new Pizza.koostisosad[] { Pizza.koostisosad.juust, Pizza.koostisosad.pepperoni, Pizza.koostisosad.tomatikaste }));
+            pizzaList.Add(new Pizza("Rukola", 5.50f, new Pizza.koostisosad[] { Pizza.koostisosad.juust, Pizza.koostisosad.rukola, Pizza.koostisosad.tomatikaste }));
+            pizzaList.Add(new Pizza("Pepperoni & Rukola", 6.00f, new Pizza.koostisosad[] { Pizza.koostisosad.juust, Pizza.koostisosad.pepperoni, Pizza.koostisosad.rukola, Pizza.koostisosad.tomatikaste }));
+        }
+        public Pizza GetPizza(string name)
+        {
+            foreach (Pizza pizza in pizzaList)
+            {
+                if (pizza.name == name)
+                    return pizza;
+            }
+            return null;
+        }
+    }
+}
