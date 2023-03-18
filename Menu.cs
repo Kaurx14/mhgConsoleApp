fb91e96940458d9c22eef44b98a7ffb714d1e779
Kaurx14 <99817804+Kaurx14@users.noreply.github.com>
2023-03-18 10:59:43 +0200

    cah

---
+++ Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mhgConsoleApp
+{
+    public class Menu
+    {
+        public List<Pizza> menu = new List<Pizza>();
+        public Menu()
+        {
+            menu.Add(new Pizza("Pepperoni", 5.00f, new Pizza.koostisosad[] { Pizza.koostisosad.juust, Pizza.koostisosad.pepperoni, Pizza.koostisosad.tomatikaste }));
+            menu.Add(new Pizza("Rukola", 5.50f, new Pizza.koostisosad[] { Pizza.koostisosad.juust, Pizza.koostisosad.rukola, Pizza.koostisosad.tomatikaste }));
+            menu.Add(new Pizza("Pepperoni & Rukola", 6.00f, new Pizza.koostisosad[] { Pizza.koostisosad.juust, Pizza.koostisosad.pepperoni, Pizza.koostisosad.rukola, Pizza.koostisosad.tomatikaste }));
+        }
+        public Pizza GetPizza(string name)
+        {
+            foreach (Pizza pizza in menu)
+            {
+                if (pizza.name == name)
+                    return pizza;
+            }
+            return null;
+        }
+    }
+}
